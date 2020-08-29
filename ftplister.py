333a6bcedeb1dfd22c88693fea469149d708d491
Anders Rosdahl <rosboll@live.se>
2020-08-29 16:51:40 +0200

    Added comments to ftplister.py

--- ftplister.py
+++ ftplister.py
@@ -4,7 +4,7 @@ from ftplib import FTP
 from sys import argv
 # from getopt import getopt
 
-
+# Read IP addresses from file
 with open(argv[1],'r') as infile:
 	# IPs = infile.readlines()
 	IPs = [line.strip() for line in infile]
@@ -12,7 +12,7 @@ infile.close()
 
 # print(IPs)
 
-
+# Log in anonymously and list contents of FTP landing folder on each IP 
 for ip in IPs:
 	ftp = FTP(ip)
 	# ftp.set_debuglevel(1)
